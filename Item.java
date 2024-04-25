78cd1bae04dbd7240d0485083e8e351d55af1d6f
amyliaKusuma <161804094+amyliaKusuma@users.noreply.github.com>
2024-04-25 10:56:47 +0700

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,5 @@
+public class Item {
+    // Implementasi atribut dan metode khusus untuk item jika diperlukan
+    // Misalnya, implementasi untuk jenis-jenis item tertentu
+}
+
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Implementasi logika permainan
+        // Misalnya, membuat karakter, senjata, item, dan menjalankan pertarungan antar karakter
+    }
+}
